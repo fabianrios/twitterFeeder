26353238da6c5168d9d127882f9fab83a039366e
fabianrios <faben02@gmail.com>
2017-06-29 12:30:44 +0200

    twitter

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var Twitter = require('twitter');
+
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/twitter', function(req, res, next) {
+  client.get('favorites/list', function(error, tweets, response) {
+    if(error) throw error;
+    console.log(tweets);  // The favorites. 
+    console.log(response);  // Raw response object. 
+    res.status(200).send(JSON.stringify(tweets));
+  });
+});
+
 
 module.exports = router;
