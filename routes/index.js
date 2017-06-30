7b84fad7e9924502056ea99422a709b888a12738
fabianrios <faben02@gmail.com>
2017-07-01 00:19:16 +0200

    brute force split string

--- routes/index.js
+++ routes/index.js
@@ -15,13 +15,38 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-function addNewlines(str) {
-  var result = '';
-  while (str.length > 0) {
-    result += str.substring(0, 50) + '\n';
-    str = str.substring(50);
-  }
-  return result;
+function splitIntoLines(input, len) {
+    var i;
+    var output = [];
+    var lineSoFar = "";
+    var temp;
+    var words = input.split(' ');
+    for (i = 0; i < words.length;) {
+        // check if adding this word would exceed the len
+        temp = addWordOntoLine(lineSoFar, words[i]);
+        if (temp.length > len) {
+            if (lineSoFar.length == 0) {
+                lineSoFar = temp;     // force to put at least one word in each line
+                i++;                  // skip past this word now
+            }
+            output.push(lineSoFar);   // put line into output
+            lineSoFar = "";           // init back to empty
+        } else {
+            lineSoFar = temp;         // take the new word
+            i++;                      // skip past this word now
+        }
+    }
+    if (lineSoFar.length > 0) {
+        output.push(lineSoFar);
+    }
+    return(output.toString());
+}
+
+function addWordOntoLine(line, word) {
+    if (line.length != 0) {
+        line += " ";
+    }
+    return(line += word);
 }
 
 router.get('/twitter/favorites', function(req, res, next) {
